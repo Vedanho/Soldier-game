dcf537dee7eb269e019860688a959ccf5b67d863
Amirhan Inasalamov <amirhan.inasalamov@mail.ru>
2022-05-18 10:49:21 +0300

    In the were added the next methods: shoot, reload, fire. Made code review.

--- Game.js
+++ Game.js
@@ -1,19 +1,78 @@
 const soldier = {
-    name: 'Musost',
-    health: 10,
-    weapon: 
-    {
-        name: 'Ak-47',
-        patrons: 30
-    },
-     provisions: 3,
-     shoot () {
-         
-     },
-     reload () {
-
-     },
-     hurt () {
-
-     }
-}
+  name: "Musost",
+  health: 10,
+  weapon: {
+    name: "Ak-47",
+    patrons: 30,
+  },
+  provisions: 3,
+  shoot() {
+    if (this.weapon.patrons > 0) {
+      this.weapon.patrons -= 1;
+      console.log("Pau-Pau");
+    } else {
+      console.log("Quality of patrons is zero.Reload!");
+    }
+  },
+  reload() {
+    if (this.provisions > 0) {
+      this.weapon.patrons = 30;
+      this.provisions -= 1;
+    }
+  },
+  hurt() {
+    if (this.health > 0) {
+      this.health -= 1;
+    } else if (this.health === 0) {
+      console.log("Soldier is dead. Game over.");
+    }
+  },
+};
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.reload();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.shoot();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
+soldier.hurt();
