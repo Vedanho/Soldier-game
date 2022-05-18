c4ea43c9874b4db48ef58fca6315d5eb157133e8
Amirhan Inasalamov <amirhan.inasalamov@mail.ru>
2022-05-18 10:29:45 +0300

    Made a model of soldier in the game

---
+++ Game.js
@@ -0,0 +1,19 @@
+const soldier = {
+    name: 'Musost',
+    health: 10,
+    weapon: 
+    {
+        name: 'Ak-47',
+        patrons: 30
+    },
+     provisions: 3,
+     shoot () {
+         
+     },
+     reload () {
+
+     },
+     hurt () {
+
+     }
+}
